6f8865f159634e211b43a02c54d9d0e5d9d6dd1e
prajwaldankit <prajwal.x82@gmail.com>
2026-09-18 21:17:52 +0545

    Add useEffect to hide splash screen after loading

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { useColorScheme } from "react-native";
 
 import AppTabs from "@/components/app-tabs";
@@ -10,6 +11,11 @@ SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <AppTabs />
